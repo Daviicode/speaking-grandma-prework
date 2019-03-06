16f1f48456861332643d3a2b40e5d0c8f955f6e3
David zhang <d.ydzdavid@yahoo.com>
2019-03-06 19:29:48 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma (string)
-  if sting != sting.upcase()
+  if sting !== sting.upcase()
     return "HUH? SPEAK UP, SONNY!"
   else
     return "NO, NOT SINCE 1938!"
