628d28038cc553bfed6737b95956a30ffa571f33
David zhang <d.ydzdavid@yahoo.com>
2019-03-06 19:31:01 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma (string)
-  if string !== string.upcase(string)
+  if string != string.upcase(string)
     return "HUH? SPEAK UP, SONNY!"
   else
     return "NO, NOT SINCE 1938!"
